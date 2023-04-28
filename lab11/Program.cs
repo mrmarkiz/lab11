b7d074f70a4bc14e93bb53674c82251b80737ad3
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-28 19:25:11 +0300

    Add odd_even extensions

--- lab11/Program.cs
+++ lab11/Program.cs
@@ -1,28 +1,27 @@
 ﻿namespace lab11
 {
-    internal class Program
+    public static class intExtensions
     {
-
-
-
-        public static void Main()
+        public static bool IsOdd(this int number)
         {
-            
+            return number % 2 == 1;
+        }
+        public static bool IsEven(this int number)
+        {
+            return number % 2 == 0;
         }
     }
-}
 
-public static class intExtensions
-{
-    public static int myConvertDolar(this int input)
+    public class Program
     {
-        return input * 8;
-    }
-}
-public class Program
-{
-    public static void Main()
-    {
-        int num = 5; Console.WriteLine(num.myConvertDolar()); Console.ReadLine();
+        public static void Main()
+        {
+            Console.Write("Enter numeber: ");
+            if(int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.WriteLine($"Odd check result: {number.IsOdd()}");
+                Console.WriteLine($"Even check result: {number.IsEven()}");
+            }
+        }
     }
 }
