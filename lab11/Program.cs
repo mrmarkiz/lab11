2f92776020f0b94ce58e35b6457fc92f9076d635
mrmarkiz <maxtomusiak315@gmail.com>
2023-04-28 19:20:35 +0300

    Add project files.

---
+++ lab11/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace lab11
+{
+    internal class Program
+    {
+
+
+
+        public static void Main()
+        {
+            
+        }
+    }
+}
+
+public static class intExtensions
+{
+    public static int myConvertDolar(this int input)
+    {
+        return input * 8;
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        int num = 5; Console.WriteLine(num.myConvertDolar()); Console.ReadLine();
+    }
+}
